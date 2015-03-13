d0d79020201d8a510898f628bea6ff00981428c8
xiang zhyuan <xiangzhuyuan@gmail.com>
2015-03-13 14:57:46 +0800

    demo

---
+++ ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/zhuyuan.xiang/workspace/python-getting-started/ENV/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ ENV/lib/python2.7/_weakrefset.py
@@ -0,0 +1,1 @@
+/Users/zhuyuan.xiang/.pyenv/versions/2.7.8/lib/python2.7/_weakrefset.py
